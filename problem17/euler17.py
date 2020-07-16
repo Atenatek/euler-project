9c02385ed147de44b51bbeb43772caeefa341825
Mael <Mael@MacBook-Pro-de-Mael-2.local>
2020-07-15 21:18:57 -0400

    problem 17 solved with f string method

--- problem17/euler17.py
+++ problem17/euler17.py
@@ -1,3 +1,13 @@
+# Number letter counts
+# Problem 17
+
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen)
+# contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
 
 numbers = {
     1:'one', 
@@ -27,10 +37,34 @@ numbers = {
     70:'seventy',
     80:'eighty',
     90:'ninety',
-    100:'one hundred',
-    1000:'one thousand'
+    100:'hundred',
     }
 
-one_to_nine = [numbers[r+1] for r in range(9)] 
+#create a list of written numbers from 1 to 19
+to_99 = [numbers[r] for r in range(1, 20)] 
 
-print(one_to_nine)
+#append 20 to 99 to the list
+for d in range(20, 100, 10):
+    to_99.append(f'{numbers[d]}')
+    for u in range(1, 10):
+        to_99.append(f'{numbers[d]} {numbers[u]}')
+
+#create a list from 100 to 999
+hundred_to_1000 = []
+for u in range(1, 10):
+    hundred_to_1000.append(f'{numbers[u]} {numbers[100]}')
+    for n in to_99 :
+        hundred_to_1000.append(f'{numbers[u]} {numbers[100]} and {n}')
+
+#append 1000 to the list
+hundred_to_1000.append('one thousand')
+
+#join the 2 lists, creating a list from 1 to 1000
+to_1000 = to_99 + hundred_to_1000
+
+#add the len of each number of the list, removing the white spaces
+n = 0
+for num in to_1000 :
+    n += len(num.replace(' ', ''))
+
+print(n)
